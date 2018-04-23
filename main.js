9304eaf3b5d45e77454d519b2c5eb6d73edbd31e
Amos <asidelnik@gmail.com>
2018-04-23 14:52:15 +0300

    ex.3 complete

--- main.js
+++ main.js
@@ -1,65 +1,36 @@
 let posts = [];
 let postNumber = 0;
 
-let newPost = function(userInput) {
+let newPost = function(userInput, postNumber) {
     let post = {};
     post.text = userInput;
     post.id = postNumber;
     posts.push(post);
-    postNumber++;// why?
     renderArray();
 }
 
 let renderArray = function() {
     $('.posts').find("p").remove();
     for (let i = 0; i < posts.length; i++) {
-      $('.posts').append($('<p class="post">' + posts[i].text + '</p>').attr('data-id', posts[i].id).append($('<button class="remove btn btn-info" type="button">Remove</button>')));
+      $('.posts').append('<p class="post" data-id="' + i + '">' + posts[i].text + '</p>');
+      $('.posts p[data-id="' + i + '"]').append('<button class="remove btn btn-info" type="button">Remove</button>');
     }
     console.log(posts);
 }
 
 $('.add-post').on('click', function() {
     let userInput = $('#post-name').val();
-    newPost(userInput);
+    newPost(userInput, postNumber);
+    postNumber++;
 })
 
+$('input:text').focus(
+    function(){
+        $(this).val('');
+    });
 
 $('.posts').on('click', '.remove',function() {
-  debugger
-    var parIndexToRemove = $(this).closest('p').attr("data-id");
-    posts.splice(parIndexToRemove, 1);
-
-     renderArray();
+    var indexToRemove = $(this).closest('p').attr("data-id");  //get object's index in the array
+    posts.splice(indexToRemove, 1);
+    renderArray();
 })
-
-
-/*
-
-// $('.posts').on('click', '.remove',function() {
-//     var parIndexToRemove = $(this).closest('p');
-//     posts.splice(parIndexToRemove, 1);            // remove object from array
-//     $(this).closest("p").remove();                // remove p from div
-//
-//     for (var i = 0; i < posts.length; i++) {
-//         posts[i].id = i;                          // change object id of index to index  / 0 to 0...
-//         $('.posts p').eq(i).attr('data-id', i);        // change p data-id attribute to index
-//     }
-//     console.log(posts);
-// })
-
-// for (var i = 0; i < posts.length; i++) {
-//     posts[i].id = i;
-//     $('.posts p').eq(i).attr('data-id', i);
-// }
-// $('.posts p:eq(i)').data('id', i);
-// $('.posts').find('p').data('id', i);
-//.css("background-color", "yellow")
-// get first p with index 0 of loop and next ones
-// change their data-id attribute
-// attr('data-id', posts[i].id);
- //.data('id');
-// $(this)
-  if(userInput && typeof userInput === "string") {
-    newPost(userInput);
-  }
-*/
