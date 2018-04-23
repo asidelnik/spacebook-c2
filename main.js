cf9fec6b4a2cddf0f1f82a4251a81b17fd76cdfe
Amos <asidelnik@gmail.com>
2018-04-23 08:22:15 +0300

    ex.3 done

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ let posts = [];
 let postNumber = 0;
 
 let newPost = function(userInput) {
-    postNumber++;
     let post = {};
     post.text = userInput;
     post.id = postNumber;
@@ -16,6 +15,7 @@ let renderArray = function() {
       $('.posts').append($('<p class="post">' + posts[i].text + '</p>').attr('data-id', posts[i].id).append($('<button class="remove btn btn-info" type="button">Remove</button>')));
     }
     console.log(posts);
+    postNumber++;
 }
 
 $('.add-post').on('click', function() {
@@ -24,13 +24,27 @@ $('.add-post').on('click', function() {
 })
 
 $('.posts').on('click', '.remove',function() {
-    $(this).closest("p").remove();
-    posts.splice($(this).closest('p').data('id') - 1, 1);
+    var parIndexToRemove = $(this).closest('p');
+    posts.splice(parIndexToRemove, 1);            // remove object from array
+    $(this).closest("p").remove();                // remove p from div
+
+    for (var i = 0; i < posts.length; i++) {
+        posts[i].id = i;                          // change object id of index to index  / 0 to 0...
+        $('.posts p').eq(i).attr('data-id', i);        // change p data-id attribute to index
+    }
     console.log(posts);
 })
 
 
 /*
+
+// $('.posts p:eq(i)').data('id', i);
+// $('.posts').find('p').data('id', i);
+//.css("background-color", "yellow")
+// get first p with index 0 of loop and next ones
+// change their data-id attribute
+// attr('data-id', posts[i].id);
+ //.data('id');
 // $(this)
   if(userInput && typeof userInput === "string") {
     newPost(userInput);
